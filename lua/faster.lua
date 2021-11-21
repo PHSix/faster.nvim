e5c02f27159c4da508a155440b74616e60661e40
PH <15917977183@163.com>
2021-11-21 18:57:06 +0800

    upgrade get os time api and add visual mode map

--- lua/faster.lua
+++ lua/faster.lua
@@ -1,16 +1,22 @@
-local vim = vim
-local fn = vim.fn
-local press_fre = 0
+local uv = vim.loop
+local api = vim.api
+local press_freq = 0
 local times = {}
 
+local get_cur_time = function()
+  return uv.hrtime() / 1000000000
+end
 local initState = function()
-  local now_time = tonumber(fn["reltimestr"](fn["reltime"]()))
+  local now_time = get_cur_time()
   times = {
     k = now_time,
     j = now_time,
     gk = now_time,
     gj = now_time
   }
+end
+local t = function(str)
+  return api.nvim_replace_termcodes(str, true, true, true)
 end
 
 initState()
@@ -20,19 +26,16 @@ local move = function(to)
     vim.cmd("normal! " .. vim.v.count .. to)
     return
   end
-  local cur_time = times[to]
-  if
-    tonumber(fn["reltimestr"](fn["reltime"]())) - cur_time > 0.1 or
-      tonumber(fn["reltimestr"](fn["reltime"]())) - cur_time < 0.01
-   then
-    press_fre = 0
+  local last_time = times[to]
+  if get_cur_time() - last_time > 0.1 or get_cur_time() - last_time < 0.01 then
+    press_freq = 0
   end
-  local step = math.floor((press_fre / 5) + 1)
+  local step = math.floor((press_freq / 5) + 1)
   if step >= 10 then
     step = 10
   end
-  times[to] = tonumber(fn["reltimestr"](fn["reltime"]()))
-  press_fre = press_fre + 1
+  times[to] = get_cur_time()
+  press_freq = press_freq + 1
   local cmd = ""
   if step ~= 1 then
     cmd = "normal! " .. step .. to
@@ -41,6 +44,25 @@ local move = function(to)
   end
   vim.cmd(cmd)
 end
+
+local vmove = function(to)
+  local last_time = times[to]
+  if get_cur_time() - last_time > 0.1 or get_cur_time() - last_time < 0.01 then
+    press_freq = 0
+  end
+  local step = math.floor((press_freq / 5) + 1)
+  if step >= 10 then
+    step = 10
+  end
+  times[to] = get_cur_time()
+  press_freq = press_freq + 1
+  if step ~= 1 then
+    return t(step .. to)
+  else
+    return t(to)
+  end
+end
 return {
-  move = move
+  move = move,
+  vmove = vmove
 }
